5c90fc678e1999c84c3b890dbd046400bb32e547
Adib <mzadib1234@gmail.com>
2025-01-03 15:56:45 +0800

    navigation for consumer

--- app/src/main/java/com/example/serviceflexapp/consumer/ConsumerProfileFragment.java
+++ app/src/main/java/com/example/serviceflexapp/consumer/ConsumerProfileFragment.java
@@ -2,21 +2,34 @@ package com.example.serviceflexapp.consumer;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.serviceflexapp.R;
 
 
 public class ConsumerProfileFragment extends Fragment {
 
+    public ConsumerProfileFragment() {
+        super(R.layout.fragment_consumer_profile);
+    }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_consumer_profile, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NavController navController = Navigation.findNavController(view);
+
+        view.findViewById(R.id.BTN_AccountInfo).setOnClickListener(v ->
+                navController.navigate(R.id.action_consumerProfileFragment2_to_consumerProfileAccountInfo));
+        view.findViewById(R.id.BTN_Ewallet).setOnClickListener(v ->
+                navController.navigate(R.id.action_consumerProfileFragment2_to_consumerProfileEWallet));
+        view.findViewById(R.id.BTN_UpdateWorkProfile).setOnClickListener(v ->
+                navController.navigate(R.id.action_consumerProfileFragment2_to_consumerProfilePaymentHistory));
     }
 }
--- app/src/main/java/com/example/serviceflexapp/consumer/ConsumerProfilePaymentHistory.java
+++ app/src/main/java/com/example/serviceflexapp/consumer/ConsumerProfilePaymentHistory.java
@@ -2,7 +2,11 @@ package com.example.serviceflexapp.consumer;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,57 +14,19 @@ import android.view.ViewGroup;
 
 import com.example.serviceflexapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ConsumerProfilePaymentHistory#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ConsumerProfilePaymentHistory extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public ConsumerProfilePaymentHistory() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConsumerProfilePaymentHistory.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ConsumerProfilePaymentHistory newInstance(String param1, String param2) {
-        ConsumerProfilePaymentHistory fragment = new ConsumerProfilePaymentHistory();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        super(R.layout.fragment_consumer_profile_payment_history);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_consumer_profile_payment_history, container, false);
+        NavController navController = Navigation.findNavController(view);
+
+        view.findViewById(R.id.IB_Previous4).setOnClickListener(v ->
+                navController.navigate(R.id.action_consumerProfilePaymentHistory_to_consumerProfileFragment2));
     }
 }
